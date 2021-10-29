cb436dd9a16c89ae71ed7576c83456322e8fc0a1
josias <josiasr.ribeiro@gmail.com>
2021-10-29 13:05:23 -0300

    click

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 
 var play = false
 
+setTimeout(() => {
+  document.body.click();
+}, 1000)
+
 document.addEventListener('mousemove', () => {
   !play && document.querySelector('#somtriste').play();
   
   play = true
 })
+
+document.addEventListener('click', () => {
+  !play && document.querySelector('#somtriste').play();
+  
+  play = true
+})
+
 
 
 document.addEventListener('touchstart', () => {
