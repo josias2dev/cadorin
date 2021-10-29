3c6c69af87aefd175276a69fdae1bc724cdc6688
josias <josiasr.ribeiro@gmail.com>
2021-10-29 13:16:06 -0300

    play

--- index.js
+++ index.js
@@ -2,15 +2,6 @@
 
 var play = false
 
-setTimeout(() => {
-  document.body.click();
-}, 1000)
-
-document.addEventListener('mousemove', () => {
-  !play && document.querySelector('#somtriste').play();
-  
-  play = true
-})
 
 document.addEventListener('click', () => {
   !play && document.querySelector('#somtriste').play();
@@ -19,15 +10,3 @@ document.addEventListener('click', () => {
 })
 
 
-
-document.addEventListener('touchstart', () => {
-  !play && document.querySelector('#somtriste').play();
-  
-  play = true
-})
-
-
-document.addEventListener('touchmove', () => {
-  !play && document.querySelector('#somtriste').play();
-  play = true
-})
