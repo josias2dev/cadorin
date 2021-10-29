94dc51d86c417296ab898615aedeef7f2dcb3cf9
josias <josiasr.ribeiro@gmail.com>
2021-10-29 14:57:20 -0300

    text

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var play = false
 
 document.addEventListener('click', () => {
   !play && document.querySelector('#somtriste').play();
-  
+  !play && (document.querySelector('.click-text').textContent = "Aumente o volume")
   play = true
 })
 
